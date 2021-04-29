b1cdc549425cdbfd34e1f13668ea69c9aeceb32f
Adamakos <adamhohmann@gmail.com>
2021-04-29 12:29:12 +0200

    Add list

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,9 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -10,6 +14,33 @@ public class JurassicPark {
         this.dbUrl = dbUrl;
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
+    }
+
+    public List<String> checkOverpopulation() {
+        String name = null;
+        List<String> result = new ArrayList<>();
+        String query = "SELECT breed FROM dinosaur WHERE actual > expected ORDER BY breed";
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                name = resultSet.getString("breed");
+                result.add(name);
+            }
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+        return result;
+    }
+
+    private Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        } catch (SQLException e) {
+            System.err.println("Database unable to connect.");
+        }
+        return connection;
     }
 
 }
